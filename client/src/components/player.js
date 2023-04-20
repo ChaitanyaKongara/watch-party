8f57dbe605ceb3ffc24d6cee889533bf934b3f4b
sreeharsha225 <harshareddynvs@gmail.com>
2023-04-20 23:41:53 +0530

    added player.js

---
+++ client/src/components/player.js
@@ -0,0 +1,190 @@
+import React, { Component } from 'react'
+import { findDOMNode } from 'react-dom'
+
+import screenfull from 'screenfull'
+import ReactPlayer from 'react-player'
+
+class player extends Component {
+  state = {
+    url: null,
+    pip: false,
+    playing: true,
+    controls: true,
+    light: false,
+    volume: 0.8,
+    muted: false,
+    played: 0,
+    loaded: 0,
+    duration: 0,
+    playbackRate: 1.0,
+    loop: false
+  }
+
+  load = url => {
+    this.setState({
+      url,
+      played: 0,
+      loaded: 0,
+      pip: false
+    })
+  }
+
+  handlePlayPause = () => {
+    this.setState({ playing: !this.state.playing })
+  }
+
+  handleStop = () => {
+    this.setState({ url: null, playing: false })
+  }
+
+  handleToggleControls = () => {
+    const url = this.state.url
+    this.setState({
+      controls: !this.state.controls,
+      url: null
+    }, () => this.load(url))
+  }
+
+  handleToggleLight = () => {
+    this.setState({ light: !this.state.light })
+  }
+
+  handleToggleLoop = () => {
+    this.setState({ loop: !this.state.loop })
+  }
+
+  handleVolumeChange = e => {
+    this.setState({ volume: parseFloat(e.target.value) })
+  }
+
+  handleToggleMuted = () => {
+    this.setState({ muted: !this.state.muted })
+  }
+
+  handleSetPlaybackRate = e => {
+    this.setState({ playbackRate: parseFloat(e.target.value) })
+  }
+
+  handleOnPlaybackRateChange = (speed) => {
+    this.setState({ playbackRate: parseFloat(speed) })
+  }
+
+  handleTogglePIP = () => {
+    this.setState({ pip: !this.state.pip })
+  }
+
+  handlePlay = () => {
+    console.log('onPlay')
+    console.log(this.player.getCurrentTime())
+    this.setState({ playing: true })
+  }
+
+  handleEnablePIP = () => {
+    console.log('onEnablePIP')
+    this.setState({ pip: true })
+  }
+
+  handleDisablePIP = () => {
+    console.log('onDisablePIP')
+    this.setState({ pip: false })
+  }
+
+  handlePause = () => {
+    console.log('onPause')
+    console.log(this.player.getCurrentTime())
+    this.setState({ playing: false })
+  }
+
+  handleSeekMouseDown = e => {
+    this.setState({ seeking: true })
+  }
+
+  handleSeekChange = e => {
+    this.setState({ played: parseFloat(e.target.value) })
+  }
+
+  handleSeekMouseUp = e => {
+    this.setState({ seeking: false })
+    this.player.seekTo(parseFloat(e.target.value))
+  }
+
+  handleProgress = state => {
+    console.log('onProgress', state)
+    // We only want to update time slider if we are not currently seeking
+    if (!this.state.seeking) {
+      this.setState(state)
+    }
+  }
+
+  handleEnded = () => {
+    console.log('onEnded')
+    this.setState({ playing: this.state.loop })
+  }
+
+  handleDuration = (duration) => {
+    console.log('onDuration', duration)
+    this.setState({ duration })
+  }
+
+  handleClickFullscreen = () => {
+    screenfull.request(findDOMNode(this.player))
+  }
+
+  renderLoadButton = (url, label) => {
+    return (
+      <button onClick={() => this.load(url)}>
+        {label}
+      </button>
+    )
+  }
+
+  ref = player => {
+    this.player = player
+  }
+
+  render () {
+    const { url, playing, controls, light, volume, muted, loop, played, loaded, duration, playbackRate, pip } = this.state
+    const SEPARATOR = ' · '
+
+    return (
+      <div className='app'>
+        <section className='section'>
+          <h1>ReactPlayer Demo</h1>
+          <input value={url || ''} type='text' placeholder='Enter URL' onChange={e => this.setState({ url: e.target.value })} />
+          <div className='player-wrapper'>
+            <ReactPlayer
+              ref={this.ref}
+              className='react-player'
+              width='100%'
+              height='100%'
+              url={url}
+              pip={pip}
+              playing={playing}
+              controls={controls}
+              light={light}
+              loop={loop}
+              playbackRate={playbackRate}
+              volume={volume}
+              muted={muted}
+              onReady={() => console.log('onReady')}
+              onStart={() => console.log('onStart')}
+              onPlay={this.handlePlay}
+              onEnablePIP={this.handleEnablePIP}
+              onDisablePIP={this.handleDisablePIP}
+              onPause={this.handlePause}
+              onBuffer={() => console.log('onBuffer')}
+              onPlaybackRateChange={this.handleOnPlaybackRateChange}
+              onSeek={e => console.log('onSeek', e)}
+              onEnded={this.handleEnded}
+              onError={e => console.log('onError', e)}
+              onProgress={this.handleProgress}
+              onDuration={this.handleDuration}
+            />
+          </div>
+          </section>
+      </div>
+    )
+  }
+}
+
+export default player
